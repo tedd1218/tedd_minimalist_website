f1e54daa1de5b4dcf8e383b635e5ebfd12d227df
Tedd Jung <tyjung@andrew.cmu.edu>
2025-07-07 23:30:30 -0700

    added arrows to photo album images

--- app/photo/page.tsx
+++ app/photo/page.tsx
@@ -43,9 +43,9 @@ export default function Photo() {
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full">
             {[...Array(6)].map((_, i) => (
               <div key={i} className="animate-pulse">
-                <div className="aspect-square bg-gray-200 dark:bg-gray-700 rounded"></div>
+                <div className="aspect-square bg-gray-200 dark:bg-gray-700"></div>
                 <div className="mt-2 space-y-2">
-                  <div className="h-6 bg-gray-200 dark:bg-gray-700 rounded w-3/4"></div>
+                  <div className="w-full h-6 bg-gray-200 dark:bg-gray-700 w-3/4"></div>
                 </div>
               </div>
             ))}
